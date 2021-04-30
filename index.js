41b27c17eace2833741c8deb7f65c24628748099
nWhitcome <nathanw315@gmail.com>
2021-04-30 00:47:05 -0500

    sucessfully created keys, security groups, and launch configurations

--- index.js
+++ index.js
@@ -17,6 +17,9 @@ app.get('/', function(request, response){
 
 io.on('connection', (socket) => {
     console.log("a user connected");
+    socket.on("makeGroup", (name, scaleGroup) => {
+        keyMake(name, socket, scaleGroup);
+    })
 })
 
 server.listen(port);
@@ -25,11 +28,141 @@ console.log("listening on port " + port);
 AWS.config.getCredentials(function(err){
     if(err) console.log(err.stack);
     else{
-        console.log("Access key: ", AWS.config.credentials.accessKeyId);
+        AWS.config.update({region: 'us-east-2'})
     }
 })
 
-var params = {
+var ec2 = new AWS.EC2();
+var autoscaling = new AWS.AutoScaling();
 
+function keyMake(keyName, socket, scaleGroup){
+    var keyParams = {
+        KeyName: keyName + "Key"
+    };
+    ec2.createKeyPair(keyParams, function(err, data){
+        if(err){
+            console.log(err.code)
+            if(err.code =='InvalidKeyPair.Duplicate'){
+                io.to(socket.id).emit("error", "Err: A key with that name already exists");
+            }
+        }
+        else{
+            io.to(socket.id).emit("getKey", {name: keyName, content: data.KeyMaterial});
+            SGMake(keyName, socket, scaleGroup)
+        }
+    })
 }
-//var ec2 = new AWS.EC2();
+
+function SGMake(groupName, socket, scaleGroup){
+    ec2.describeVpcs(function(err, data){
+        if(err) {
+            io.to(socket.id).emit("error", "Err: Cannot retrieve a VPC");
+        }
+        else{
+            vpc = data.Vpcs[0].VpcId;
+            var paramsSecurityGroup = {
+                Description: 'Security group for ' + groupName,
+                GroupName: groupName + "SG",
+                VpcId: vpc
+            };
+            ec2.createSecurityGroup(paramsSecurityGroup, function(err, data){
+                if(err) {
+                    io.to(socket.id).emit("error", "Err: Could not make securty group");
+                }
+                else{
+                    var SecurityGroupId = data.GroupId;
+                    console.log("Success", SecurityGroupId)
+                    var paramsIngress = {
+                        GroupId: SecurityGroupId,
+                        IpPermissions:[
+                            {
+                                IpProtocol: "tcp",
+                                FromPort: 22,
+                                ToPort: 22,
+                                IpRanges: [{"CidrIp":"0.0.0.0/0"}]
+                            },
+                            {
+                                IpProtocol: "tcp",
+                                FromPort: 80,
+                                ToPort: 80,
+                                IpRanges: [{"CidrIp":"0.0.0.0/0"}]
+                            }
+                        ]
+                    };
+                    ec2.authorizeSecurityGroupIngress(paramsIngress, function(err, data){
+                        if(err){
+                            io.to(socket.id).emit("error", "Err: Ingress set failed");
+                        }
+                        else{
+                            console.log("Ingress successfully set", data);
+                            LCMake(groupName, socket, SecurityGroupId, scaleGroup);
+                        }
+                    })
+                }
+            })
+        }
+    })
+}
+
+function LCMake(LCName, socket, SGId, scaleGroup){
+    var LCParams = {
+        ImageId: "ami-e81b308d",
+        UserData: "",
+        LaunchConfigurationName: LCName + "LC",
+        InstanceType: "t2.micro",
+        SecurityGroups: [SGId],
+        KeyName: LCName + "Key"
+    }
+    autoscaling.createLaunchConfiguration(LCParams, function(err, data){
+        if(err){
+            console.log(err.stack);
+            io.to(socket.id).emit("error", "Err: Couldn't create launch configuration");
+        }
+        else{
+            console.log(data);
+        }
+    })
+}
+
+function ASMake(ASName, socket, scaleGroup){
+    var ASParams = {
+        AutoScalingGroupName: ASName + "AS",
+        HealthCheckGracePeriod: 300,
+        LaunchConfigurationName: ASName + "LC",
+        MaxSize: scaleGroup.maxScale,
+        MinSize: 1,
+        VPCZoneIdentifier: "subnet-c918b4a0, subnet-a018ebdb, subnet-25584e6f",
+        DesiredCapacity: scaleGroup.desired
+    }
+    autoscaling.createAutoScalingGroup(ASParams, function(err){
+        if(err){
+            io.to(socket.id).emit("error", "Err: Couldn't create auto scaling group");
+        }
+        else{
+            console.log("Auto scaling group created")
+        }
+    })
+}
+
+function scalingPolicyMake(SPName, socket){
+    var SPParams = {
+        AutoScalingGroupName: SPName + "AS",
+        PolicyName: SPName + "SP",
+        PolicyType: "TargetTrackingScaling",
+        EstimatedInstanceWarmup: 100,
+        TargetTrackingConfiguration: {
+            PredefinedMetricSpecification: {
+                PredefinedMetricType: "ASGAverageCPUUtilization",
+            },
+            TargetValue: 50.0
+        }
+    };
+    autoscaling.putScalingPolicy(SPParams, function(err, data) {
+        if(err){
+            io.to(socket.id).emit("error", "Err: Couldn't add scaling policy to group");
+        }
+        else{
+            console.log("Scaling policy added")
+        }
+    })
+}
--- public/app.js
+++ public/app.js
@@ -1,22 +1,90 @@
 var socket = io();
 
 const store = new Vuex.Store({
+    state: {
+        modal: {
+            show: false,
+            content: ""
+        }
+    },
+    mutations: {
+        toggleModal(state, newContent, newButton){
+            state.modal.content = newContent;
+            state.modal.buttonText = newButton;
+            state.modal.show = !state.modal.show;
+        }
+    }
 })
 
 Vue.component('custom-button', {
-    props: ['text-val'],
+    props: ['textVal'],
     template: `
-        <div class="custom-button">{{text-val}} Test</div>
+    <div class="custom-button no-select">{{textVal}}</div>
     `
 })
+
+Vue.component('modal', {
+    methods: {
+        closeModal: function(){
+            store.commit('toggleModal', "", "");
+        }
+    },
+    template: `
+    <div id="modal-back">
+        <div id="modal">
+            <p>{{this.$store.state.modal.content}}</p>
+            <custom-button @click.native="closeModal()" textVal="Ok"></custom-button>
+        </div>
+    </div>
+    `
+})
+
+socket.on("error", (errorMessage) => {
+    store.commit('toggleModal', errorMessage)
+})
+
+socket.on("getKey", (fileInfo) => {
+    downloadFile(fileInfo.name + ".pem", fileInfo.content);
+    store.commit('toggleModal', "IMPORTANT: The private key required to access the created instance has begun its download. Do not lose this key or share it with anyone.")
+})
+
+function downloadFile(filename, text){
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
 
 new Vue({
     el:'#app',
     store,
+    data: function(){
+        return {
+            nameMessage: null,
+            maxScale: null,
+            desired: null
+        }
+    },
+    methods:{
+        createLaunchConfig: function(nameValue, maxScale, desired){
+            socket.emit("makeGroup", nameValue, {max: maxScale, desired: desired});
+        },
+    },
     template:`
         <div id="app-area">
-            <h1>Hello there. I just started this project. Nothing to see here.</h1>
-            <custom-button text-val="Create Instance"></custom-button>
+            <div id="middle-box"> 
+                <input v-model="nameMessage" placeholder="Instance name">
+                <input v-model="maxScale" placeholder="Max scaling size">
+                <input v-model="desired" placeholder="Desired scale capacity">
+                <custom-button @click.native="createLaunchConfig(nameMessage, maxScale, desired)" text-val="Create Instance" ></custom-button>
+            </div>
+            <modal v-if="this.$store.state.modal.show"></modal>
         </div>
     `
 })
